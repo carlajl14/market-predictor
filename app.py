97bc2e57cfb3def6c8e4358d81fcf1ad6d29e0aa
carlajl14 <113455597+carlajl14@users.noreply.github.com>
2025-03-10 09:29:59 +0000

    Add os

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from data_sources import obtener_datos_activos, obtener_datos_multiples_activos  # Importar las funciones para obtener datos
+import os
 
 app = Flask(__name__)
 
